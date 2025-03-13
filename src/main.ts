fd2529a5a999235e89a3217415694bac1a78e083
lara huang <lara1105huang@gmail.com>
2025-03-13 15:58:14 +0800

    auto

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/router'
 import './assets/scss/allStyle.scss'
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from "node:url"
 import { resolve } from "path"
-
+import Pages from 'vite-plugin-pages';
 export default defineConfig({
   base: '/',
   plugins: [
     vue(),
+    Pages({
+      dirs: [
+         { dir: 'src/views/', baseRoute: '/' },
+      ]
+    })
   ],
   resolve: {
     alias: {
